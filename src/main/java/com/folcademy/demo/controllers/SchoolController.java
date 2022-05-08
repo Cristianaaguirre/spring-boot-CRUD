30ea7c00b051dcb69b37c10acf5b4e9b7d1ee457
Cristian Aguirre <cristianaaguirre0@gmail.com>
2022-05-08 10:48:31 -0300

    modificaciones en schoolController

--- src/main/java/com/folcademy/demo/controllers/SchoolController.java
+++ src/main/java/com/folcademy/demo/controllers/SchoolController.java
@@ -1,5 +1,6 @@
 package com.folcademy.demo.controllers;
 
+import com.folcademy.demo.exceptions.MyException;
 import com.folcademy.demo.models.School;
 import com.folcademy.demo.services.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +26,7 @@ public class SchoolController {
    }
 
    @GetMapping(path = "/get-one-school/{id}")
-   public ResponseEntity<School> getSchool(@PathVariable("id") String auxId) {
+   public ResponseEntity<School> getSchool(@PathVariable("id") String auxId){
       return ResponseEntity
          .status(HttpStatus.OK)
          .body(schoolService.getSchool(auxId));
@@ -50,28 +51,28 @@ public class SchoolController {
    }
 
    @PatchMapping(path = "/add-student/student/{id-st}/school/{id-sch}")
-   public ResponseEntity<School> addStudent(@PathVariable("id-st") String idst, @PathVariable("id-sch") String idsb) throws Exception {
+   public ResponseEntity<School> addStudent(@PathVariable("id-st") String idst, @PathVariable("id-sch") String idsb) throws MyException {
       return ResponseEntity
          .status(HttpStatus.OK)
          .body(schoolService.addStudent(idst, idsb));
    }
 
    @PatchMapping(path = "/remove-student/school/{id-sc}/student/{id-st}")
-   public ResponseEntity<School> removeStudent(@PathVariable("id-sc") String idsc, @PathVariable("id-st") String idst) throws Exception {
+   public ResponseEntity<School> removeStudent(@PathVariable("id-sc") String idsc, @PathVariable("id-st") String idst) throws MyException {
       return ResponseEntity
          .status(HttpStatus.NO_CONTENT)
          .body(schoolService.deleteStudent(idsc, idst));
    }
 
    @PatchMapping(path = "/add-professor/professor-{id-pr}/school-{id-sch}")
-   public ResponseEntity<School> addProfessor(@PathVariable("id-pr") String idpr, @PathVariable("id-sch") String idsch) throws Exception {
+   public ResponseEntity<School> addProfessor(@PathVariable("id-pr") String idpr, @PathVariable("id-sch") String idsch) throws MyException {
       return ResponseEntity
          .status(HttpStatus.OK)
          .body(schoolService.addProfessor(idpr, idsch));
    }
 
    @PatchMapping(path = "/remove-professor/professor-{id-pr}/school-{id-sch}")
-   public ResponseEntity<School> removeProfessor(@PathVariable("id-pr") String idpr, @PathVariable("id-sch") String idsch) throws Exception {
+   public ResponseEntity<School> removeProfessor(@PathVariable("id-pr") String idpr, @PathVariable("id-sch") String idsch) throws MyException {
       return ResponseEntity
          .status(HttpStatus.NO_CONTENT)
          .body(schoolService.deleteProfessor(idpr, idsch));
