8c4ad460308d2694c4df117f5dc14734dfca6242
Cristian Aguirre <cristianaaguirre0@gmail.com>
2022-05-09 10:11:51 -0300

    agregando cambios en las excepciones, correcciones minimas de espacio

--- src/main/java/com/folcademy/demo/exceptions/MyException.java
+++ src/main/java/com/folcademy/demo/exceptions/MyException.java
@@ -1,7 +1,6 @@
 package com.folcademy.demo.exceptions;
 
 public class MyException extends Exception{
-
    public MyException(String aux) {
       super(aux);
    }
--- src/main/java/com/folcademy/demo/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/folcademy/demo/exceptions/ResourceNotFoundException.java
@@ -1,19 +1,19 @@
 package com.folcademy.demo.exceptions;
 
-public class ResourceNotFoundException extends RuntimeException{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.NO_CONTENT)
+public class ResourceNotFoundException extends RuntimeException{
    public ResourceNotFoundException(String message) {
       super(message);
    }
-
    public ResourceNotFoundException(String messageFormat, Object... args) {
       super(String.format(messageFormat, args));
    }
-
    public ResourceNotFoundException(String id, String entity) {
       super("No " + entity + " found with ID " + id);
    }
-
    public ResourceNotFoundException() {
    }
 }
